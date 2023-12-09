4ddfd26ab91eb9d1a46f1603613d77e7612c7a6b
George Astakhov <goha1115@mail.ru>
2023-12-09 23:18:23 +0300

    changes in urls.py

---
+++ catalog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import start_form, contacts, products
+
+# Конфигурационное имя приложения
+app_name = CatalogConfig.name
+
+# Ссылки на страницы приложения Catalog
+urlpatterns = [
+    path('', start_form, name='start_form'),
+    path('contacts/', contacts, name='contacts'),
+    path('<int:pk>products/', products, name='products'),
+]
