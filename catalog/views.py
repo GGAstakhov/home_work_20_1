cedb4c248f4003b3e4c0c6e7d5dc7d9276ac320b
George Astakhov <goha1115@mail.ru>
2023-12-09 23:04:33 +0300

    added some changes in files

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Category, Product
+
+
+def start_form(request):
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Склад товаров',
+        'view': 'Просмотр товаров',
+    }
+    return render(request, 'catalog/start_form.html', context)
+
+
+# Страница с контактными данными
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'name: {name}, email: {email}, message: {message}')
+        return render(request, 'catalog/contacts.html')
+
+
+# Страница с продуктами по выбранным категориям
+def products(request, pk):
+    context = {
+        'object_list': Product.objects.filter(category=pk),
+        'title': 'Склад товаров',
+        'view': 'Купить товар',
+    }
+    return render(request, 'catalog/products.html', context)
--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import start_form, contacts, products
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Конфигурационное имя приложения
+app_name = CatalogConfig.name
+
+# Ссылки на страницы приложения Catalog
+urlpatterns = [path('', start_form, name='start_form'),
+    path('contacts/', contacts, name='contacts'),
+    path('<int:pk>products/', products, name='products'),
+]
